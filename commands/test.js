09fe8bfce528e8df21ea480a96117530e9936e04
rick08180818 <rick08180818@gmail.com>
2023-02-21 00:14:22 +0800

    command run ok

--- commands/test.js
+++ commands/test.js
@@ -2,10 +2,9 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('Replies with Pong!'),
+		.setName('test')
+		.setDescription('test bot'),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+		await interaction.reply('test success!');
 	},
 };
-
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs')
 const path = require('node:path')
-const {Client, Events, GatewayIntentBits} = require('discord.js')
+const {Client, Events, GatewayIntentBits, Collection} = require('discord.js')
 const {token} = require('./config.json')
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] })
@@ -13,7 +13,6 @@ const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
 	const command = require(filePath);
-	// Set a new item in the Collection with the key as the command name and the value as the exported module
 	if ('data' in command && 'execute' in command) {
 		client.commands.set(command.data.name, command);
 	} else {
