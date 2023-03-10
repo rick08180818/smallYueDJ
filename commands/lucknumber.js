c112d5fc251cd5f7579eded10eb03ebd8bf9fe52
rick08180818 <rick08180818@gmail.com>
2023-03-10 15:27:06 +0800

    command update

--- commands/lucknumber.js
+++ commands/lucknumber.js
@@ -13,16 +13,31 @@ async execute(interaction){
     const computernumber = Math.floor(Math.random() * 100) + 1
     const usernumber = interaction.options.getNumber('lucknumber')
     const imagesurl = getRandomImageUrl()
+    // const computernumber = 22
+
 
     function getRandomImageUrl() {
         return images[Math.floor(Math.random() * images.length)];
     }      
 
+if(interaction.guildId == "977115902338297906"){
+    if(interaction.channel.id == "1083631894807195648" || interaction.channel.id == "1007639350323986472"){
+        if(usernumber == computernumber){
+        interaction.reply("你獲得了一張色圖，淦死變態\n"+(imagesurl))
+        }else{
+        interaction.reply(`${interaction.member}，我的數字是${computernumber}，真可惜～`)
+        }
+    }else{
+        interaction.reply(`${interaction.member}，請在賭博區內使用喔～`)
+    }
+}else{
     if(usernumber == computernumber){
         interaction.reply("你獲得了一張色圖，淦死變態\n"+(imagesurl))
     }else{
         interaction.reply(`${interaction.member}，我的數字是${computernumber}，真可惜～`)
     }
+}
+
 
     }   
 }
