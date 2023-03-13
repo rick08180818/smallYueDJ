fdfc526069d2b430c385899b28273d723eb372c4
rick08180818 <rick08180818@gmail.com>
2023-03-13 13:09:51 +0800

    bonusDices updata

---
+++ commands/rc_beta.js
@@ -0,0 +1,75 @@
+const {SlashCommandBuilder} = require('discord.js')
+
+module.exports = {
+    data : new SlashCommandBuilder()
+    .setName('roll_check_beta')
+    .setDescription('Roll COC skill check')
+    .addNumberOption(option => option
+        .setName('skill_leves')
+        .setDescription('PC skill leves')
+        .setRequired(true)
+    )
+    .addStringOption(option => option
+        .setName('skill_name')
+        .setDescription('skill name')
+    )
+    .addNumberOption(option => option
+        .setName('bonus_dice')
+        .setDescription('Number of bonus dice')),
+
+    async execute(interaction){
+        const skill = interaction.options.getNumber('skill_leves')
+        const diceRolls = []
+        const bonusDices = []
+        const skillname = interaction.options.getString('skill_name')
+        const bonusDice = interaction.options.getNumber('bonus_dice') || 0;
+
+        let resultMessage
+        
+        for (let i = 0; i < 1 ; i++) {
+            diceRolls.push(Math.floor(Math.random() * 100) + 1);
+        }
+
+        for (let i = 0; i < bonusDice; i++) {
+            bonusDices.push(Math.floor(Math.random() * 100) + 1);
+        }
+
+        const loss = diceRolls.filter(roll => roll > skill).length
+        const regularSuccess = diceRolls.filter(roll => roll <= skill && roll > Math.floor(skill / 2)).length
+        const hardSuccess = diceRolls.filter(roll => roll <= Math.floor(skill / 2) && roll > Math.floor(skill / 5)).length
+        const extremeSuccess = diceRolls.filter(roll => roll <= Math.floor(skill / 5) && roll > 5).length
+        const bigSuccess = diceRolls.filter(roll => roll <= 5 && roll <= skill).length
+        const fumble = diceRolls.filter(roll => roll >= 96 && roll > skill).length
+
+        const bonusDicesloss = bonusDices.filter(roll => roll > skill).length
+        const bonusDicesregularSuccess = bonusDices.filter(roll => roll <= skill).length
+        
+        if(skillname){
+            if(bonusDices){resultMessage = `🎲${interaction.member}進行了“${skillname}”檢定(技能值 ${skill}):${diceRolls.join(', ')},${bonusDices.join(', ')}\n`}else{resultMessage = `🎲${interaction.member}進行了“${skillname}”檢定(技能值 ${skill}):${diceRolls.join(', ')}\n`}
+        }else{
+            if(bonusDices){resultMessage = `🎲${interaction.member}進行了檢定(值 ${skill}):${diceRolls.join(', ')},${bonusDices(', ')}\n`}else{resultMessage = `🎲${interaction.member}進行了“${skillname}”檢定(技能值 ${skill}):${diceRolls.join(', ')}\n`}
+        }
+
+        if (regularSuccess > 0 || bonusDicesregularSuccess > 0) {
+            interaction.reply(resultMessage + `普通成功`)
+        }
+        if (hardSuccess > 0) {
+            interaction.reply(resultMessage + `困難成功`)
+        }
+        if (extremeSuccess > 0) {
+            interaction.reply(resultMessage + `哦～極限成功`)
+        }
+        if (bigSuccess > 0) {
+            interaction.reply(resultMessage + `誒！噔噔噔～大成功！`)
+        }
+        if (loss > 0 && bonusDicesloss > 0) {
+            interaction.reply(resultMessage + `噔噔咚～，大失敗`)
+        }
+        if(fumble > 0 && bonusDicesregularSuccess <= 0){
+            interaction.reply(resultMessage + `噔噔咚～，大失敗`)
+        }
+        if (loss > 0 && bonusDicesregularSuccess <= 0){
+            interaction.reply(resultMessage + "ㄨㄚˊ～失敗")
+        }
+    }
+}
